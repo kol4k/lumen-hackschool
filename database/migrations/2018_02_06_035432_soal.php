199bdd8b150d8d1524e09410a5bb3db8ee4c68b5
kol4k <rafinyadi@gmail.com>
2018-02-06 11:16:11 +0700

    add migration soal and seeder soal rpl

---
+++ database/migrations/2018_02_06_035432_soal.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Soal extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soal', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pelajaran_id')->unsigned();
+            $table->foreign('pelajaran_id')->references('id')->on('pelajaran');
+            $table->text('soal');
+            $table->string('gambar_soal',251)->nullable();
+            $table->text('a');
+            $table->text('b');
+            $table->text('c');
+            $table->text('d');
+            $table->text('e');
+            $table->string('jawaban',2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soal');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -170,5 +170,95 @@ class DatabaseSeeder extends Seeder
                 'pelajaran_id' => 3,
             ],
         ]);
+        DB::table('soal')->insert([
+            [
+                'soal' => 'HTML merupakan singkatan dari',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => 'Home Tool  Markup Language',
+                'b' => 'Hyperlinks and Text Markup Language',
+                'c' => 'Hyper Text Markup Language',
+                'd' => 'Hyper Tool Markup Language',
+                'e' => 'Hyper Tricks Markup Language',
+                'jawaban' => 'c'
+            ],
+            [
+                'soal' => 'Siapa yang mengembangkan Sejarah Web pertama kali',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => 'Ruben',
+                'b' => 'Thomas Alpha Edison',
+                'c' => 'Tim Berners-Lee',
+                'd' => 'Albert Einstein',
+                'e' => 'Steward',
+                'jawaban' => 'c'
+            ],
+            [
+                'soal' => 'Profesi dalam pengembangan web, kecuali',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => 'Web Developer',
+                'b' => 'Web Programer',
+                'c' => 'Web Designer',
+                'd' => 'Web Administrator',
+                'e' => 'Web Browser',
+                'jawaban' => 'e'
+            ],
+            [
+                'soal' => 'Pada tanggal brapa www dapat di gunakan gratis',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => '20 april 1993',
+                'b' => '27 april 1993',
+                'c' => '20 april 1939',
+                'd' => '25 april 1995',
+                'e' => '30 april 1993',
+                'jawaban' => 'e'
+            ],
+            [
+                'soal' => 'Yang dimaksud dengan halaman Web adalah',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => 'Halaman elektronik yang dibuka dengan email',
+                'b' => 'Halaman online bisa di download',
+                'c' => 'Halaman digital yang dibuka dengan web browser',
+                'd' => 'Halaman digital yang berisi berbagai jenis data dan gambar',
+                'e' => 'Digital online yang terhubung dengan internet',
+                'jawaban' => 'c'
+            ],
+            [
+                'soal' => 'Tahun berapa web di buat?',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => '1993',
+                'b' => '1999',
+                'c' => '1945',
+                'd' => '1993',
+                'e' => '1991',
+                'jawaban' => 'c'
+            ],
+            [
+                'soal' => 'Kegiatan menulis html di sebut?',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => 'Nulis',
+                'b' => 'Coding',
+                'c' => 'Ngetik',
+                'd' => 'Gambar',
+                'e' => 'Melukis',
+                'jawaban' => 'c'
+            ],
+            [
+                'soal' => 'Apa kepanjangan WWW?',
+                'gambar_soal' => null,
+                'pelajaran_id' => 3,
+                'a' => 'World Wide Web',
+                'b' => 'Wide World Web',
+                'c' => 'Web Wide World',
+                'd' => 'World Web Wide',
+                'e' => 'Web World Wide',
+                'jawaban' => 'a'
+            ],
+        ]);
     }
 }
